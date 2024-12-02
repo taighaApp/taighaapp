8b99d14a85e8c68ca19a23ad10ae48e9da6d07a9
ps dinesh <psdinesh@taigha.com>
2024-12-02 10:38:00 +0530

    confilct issue fixed

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,13 +5,9 @@ import HomeSearchMap from '@/components/HomeSearch/HomeSearchMap';
 import PropertyListView from '@/components/common/PropertyListView';
 import HomeSearchCardview from '@/components/HomeSearch/HomeSearchCardViews';
 import BatchActionModel from '@/components/HomeSearch/BatchActionModel';
-<<<<<<< HEAD
 import CustomBottomSheet from '@/components/common/CustomBottomSheet';
 import FindProperties from '@/components/HomeSearch/FindProperties';
 import CustomBottomsheetModel from '@/components/common/CustomBottomsheetModel';
-=======
-import PropertiesDetails from '@/components/HomeSearch/PropertiesDetails';
->>>>>>> c15aef56ecd953da56accc1278be09b157ab44d2
 
 // Placeholder screen components
 function DashboardScreen() {
@@ -51,22 +47,7 @@ function MoreScreen() {
   
   return (
     <View style={{flex:1}}>
-<<<<<<< HEAD
       <FindProperties/>
-=======
-   {/* <Pressable style={styles.checkboxWrapper} onPress={toggleCheckbox}>
-                  <Checkbox 
-                  style={styles.checkbox}
-                  value={isChecked} 
-                  color={isChecked ? '#3366cc' : ''}
-                  onValueChange={setChecked} />
-                    <Text style={{}}>Selected</Text>
-              </Pressable> */}
-      {/* <CustomBottomSheet/> */}
-      {/* <BatchActionModel/> */}
-      {/* <Model/> */}
-      {/* <PropertiesDetails/> */}
->>>>>>> c15aef56ecd953da56accc1278be09b157ab44d2
     </View>
   );
 }
